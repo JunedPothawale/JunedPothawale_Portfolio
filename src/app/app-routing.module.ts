f44155e32cfcaf590c5fbfa15cf638b3bbf03f8e
Juned Pothawale <junedwebsole@gmail.com>
2023-12-21 07:11:07 +0530

    Navbar Is Ready For PC Versions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponentComponent } from './error-component/error-component.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import('./landing/landing.module').then((m) => m.LandingModule),
+  }, {
+    path: "dashboard",
+    loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule)
+  },
+  {
+    path: "**", component: ErrorComponentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorComponentComponent } from './error-component/error-component.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponentComponent
   ],
   imports: [
     BrowserModule,
